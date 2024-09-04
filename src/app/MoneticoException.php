f27339f0f8525fd5a11f1868e28ce6397ff5b508
B1nj <postmaster@b1nj.fr>
2024-09-03 20:40:11 -0400

    fix MoneticoException.php

--- src/app/MoneticoException.php
+++ src/app/MoneticoException.php
@@ -15,6 +15,6 @@ class MoneticoException extends \Exception
     public function __construct($message = "", $code = 0, \Throwable $previous = null)
     {
         parent::__construct($message, $code, $previous);
-        \Log::channel('monetico')->alert($message);
+        \Log::channel(config('monetico.logging_channel'))->alert($message);
     }
 }
